6484a2af6f4b6fa2be1c0102148892d61fe14b67
Michael Gutierrez <findmagpie@gmail.com>
2025-09-24 23:26:25 -0700

    fix setup script

--- nhost/migrations/default/01_create_initial_schema.sql
+++ nhost/migrations/default/01_create_initial_schema.sql
@@ -1,7 +1,7 @@
 -- File: 01_create_initial_schema.sql
 
 CREATE TABLE public.posts (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   notion_page_id TEXT UNIQUE,
   slug TEXT UNIQUE NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
